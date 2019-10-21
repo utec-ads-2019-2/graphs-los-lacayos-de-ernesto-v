16f6465b9e8be3832b0301db3a24c9c7ef758f98
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-10-21 00:06:31 -0500

    Update nodos.h

--- nodos.h
+++ nodos.h
@@ -1,13 +1,19 @@
-//
-// Created by saidchoquehuanca on 9/10/2019.
-//
 
 #ifndef UNTITLED7_NODOS_H
 #define UNTITLED7_NODOS_H
 
 #include "string.h"
 
-
+template <typename G>
+class Node {
+public:
+    typedef typename G::N N;
+    typedef typename G::Edge Edge;
+    typedef typename G::EdgeSeq EdgeSeq;
+    EdgeSeq edges;
+private:
+    N data;
+};
 
 
 #endif //UNTITLED7_NODOS_H
