96b67f8410887cb504bb791d961e81ea126926f9
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-10-21 00:06:09 -0500

    Update Edge.h

--- Edge.h
+++ Edge.h
@@ -1,8 +1,24 @@
-//
-// Created by saidchoquehuanca on 9/10/2019.
-//
 
 #ifndef UNTITLED7_EDGE_H
 #define UNTITLED7_EDGE_H
+
+#include "nodos.h"
+#include <vector>
+#include <list>
+using namespace std;
+
+template <typename G>
+class Edge {
+public:
+    typedef typename G::E E;
+    typedef typename G::Node Node;
+    Node* nodes[2];
+private:
+    E data;
+};
+
+
+
+
 
 #endif //UNTITLED7_EDGE_H
