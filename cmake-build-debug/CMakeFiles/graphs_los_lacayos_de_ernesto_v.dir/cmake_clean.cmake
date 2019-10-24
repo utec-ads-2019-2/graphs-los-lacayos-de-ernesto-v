f6a5f96de13bce20dd50d708ab69f87495c3e2d4
mateonoel2 <mateo.noel@utec.edu.pe>
2019-10-24 18:42:50 -0500

    K

---
+++ cmake-build-debug/CMakeFiles/graphs_los_lacayos_de_ernesto_v.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphs_los_lacayos_de_ernesto_v.dir/main.cpp.o"
+  "bin/graphs_los_lacayos_de_ernesto_v.pdb"
+  "bin/graphs_los_lacayos_de_ernesto_v"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphs_los_lacayos_de_ernesto_v.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 using json = nlohmann::json;
 
 int main(){
-    ifstream ifs("/Users/saidchoquehuanca/ClionProjects/untitled7/airports.json");
+    ifstream ifs("/Users/m.noel/Desktop/Proyecto/graphs-los-lacayos-de-ernesto-v/airports.json");
     json json1 =json::parse(ifs);
     const json::value_type& airports= json1;
     const json::value_type& destinations= json1[0]["destinations"];
