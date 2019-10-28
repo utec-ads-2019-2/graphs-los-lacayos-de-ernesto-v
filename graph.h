a6b6a28393f7be983015c507e297f83dabe7e65f
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-10-27 20:43:32 -0500

    Update graph.h

--- graph.h
+++ graph.h
@@ -13,9 +13,13 @@ class Graph {
     map<int,Node<T>*>*nodos;
     bool isDigrafo;
     bool isConexo;
+    bool isBipartito;
+    double densidad;
+    int Aristas;
 public:
     Graph(){
         nodos = new map<int,Node<T>*>;
+        Aristas=0;
     }
 
     void printGraph(){
@@ -40,6 +44,7 @@ public:
                     auto temp2 = *it2;
                     if(temp2->get_To() == deleteNode || temp2->get_From() == deleteNode){
                         vectemp.push_back(temp2);
+                        Aristas--;
                     }
                 }
                 for(int i = 0; i < vectemp.size();i++){
@@ -51,8 +56,25 @@ public:
         nodos->erase(id);
         delete deleteNode;
     }
-    void Size(){
+    bool removeEdge(Node<T>* from, Node<T>* to){
+        auto temp = buscarArista(from,to);
+        if(temp){
+            auto temp2 = from->getAristas();
+            temp2->remove(temp);
+            delete temp;
+            Aristas--;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    int Size(){
         cout << nodos->size();
+    }
+
+    void AumtentarArista(){
+        Aristas++;
     }
     void printAristas(){
         for(auto it = nodos->cbegin(); it!= nodos->cend();it++){
@@ -78,8 +100,7 @@ public:
             auto lista = temp->getAristas();
             for(auto it = lista->cbegin(); it!= lista->cend();it++){
                 auto temp2 = *it;
-                auto temp3 =temp2->get_From();
-                if(temp2->get_From() == from){
+                if(temp2->get_To() == to){
                     return temp2;
                 }
             }
@@ -95,16 +116,37 @@ public:
     bool fuerteconexo(){
 
     }
-
+    int get_Aristas(){
+        return Aristas;
+    }
     bool bipartito(){
 
     }
+    void calculateDensity(){
+        auto temp = Size();
+        if(temp <= 1){
+            densidad = 0;
+        }
+        else{
+            densidad = Aristas/(temp*(temp-1));
+        }
+    }
 
     ~Graph(){
-        for(auto it = nodos->cbegin(); it!=nodos->cend(); it++){
-
+        /*for(auto it = nodos->cbegin(); it!= nodos->cend(); it++){
+            auto temp = it->second;
+            auto temp2 = temp->getAristas();
+            for(auto i = temp2->cbegin(); i != temp2->cend(); i++){
+                delete *i;
+            }
         }
+        for(auto it = nodos->cbegin(); it!= nodos->cend(); it++){
+            auto temp = it->second;
+            delete temp;
+        }
+        delete nodos;*/
     }
 };
 
 #endif
+
