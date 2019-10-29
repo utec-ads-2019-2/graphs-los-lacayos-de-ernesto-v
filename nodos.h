c04453181d5fc04aac0e42a6f8c8dc16577a8b83
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-10-29 18:32:07 -0500

    Update nodos.h

--- nodos.h
+++ nodos.h
@@ -1,19 +1,62 @@
-
 #ifndef UNTITLED7_NODOS_H
 #define UNTITLED7_NODOS_H
 
+#include <list>
 #include "string.h"
+#include "Edge.h"
+using namespace std;
+template <typename A>
+class Edge;
 
-template <typename G>
+template <typename E>
 class Node {
-public:
-    typedef typename G::N N;
-    typedef typename G::Edge Edge;
-    typedef typename G::EdgeSeq EdgeSeq;
-    EdgeSeq edges;
 private:
-    N data;
+    E Data;
+    char color;
+    int id;
+    std::list<Edge<E>*>* aristas;
+public:
+
+    Node(E valor){
+        aristas = new list<Edge<E>*>;
+        Data = valor;
+        color = 'B';
+    }
+    Node(Node<E>* a){
+        Data = a->getData();
+        color = a->getColor();
+        id = a->getId();
+        aristas = new list<Edge<E>*>;
+    }
+    void insertEdge(Edge<E>*to){
+        aristas->push_back(to);
+    }
+    E getData(){
+        return Data;
+    }
+    list<Edge<E>*>* getAristas(){
+        return aristas;
+    };
+    void print_aristas(){
+        for(auto it = aristas->cbegin(); it !=  aristas->cend(); it++){
+            auto temp2 = *it;
+            cout <<  temp2->getWeight() << " ";
+        }
+    }
+    void setId(int a){
+        id = a;
+    }
+    int getId(){
+        return id;
+    }
+    ~Node(){
+        delete aristas;
+    }
+    char getColor(){
+        return color;
+    }
+    void setColor(char a){
+        color = a;
+    }
 };
-
-
-#endif //UNTITLED7_NODOS_H
+#endif
