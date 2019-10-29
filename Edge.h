2b763c158642d32fe07632c1535692aed46496d5
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-10-29 18:31:41 -0500

    Update Edge.h

--- Edge.h
+++ Edge.h
@@ -1,24 +1,51 @@
 
 #ifndef UNTITLED7_EDGE_H
 #define UNTITLED7_EDGE_H
-
 #include "nodos.h"
-#include <vector>
+
 #include <list>
 using namespace std;
+template <typename A>
+class Node;
 
-template <typename G>
+template <typename E>
 class Edge {
-public:
-    typedef typename G::E E;
-    typedef typename G::Node Node;
-    Node* nodes[2];
 private:
-    E data;
+    Node<E>* from;
+    Node<E>* to;
+    double peso;
+    bool taken;
+public:
+    Edge(){
+        from = nullptr;
+        to = nullptr;
+        peso = 0;
+        taken = false;
+    }
+    Edge(Node<E>* _from, Node<E>* _to) {
+        from = _from;
+        to = _to;
+        taken = false;
+    }
+    void set_taken(){
+        taken = true;
+    }
+    bool get_taken(){
+        return taken;
+    }
+    void setWeight(double x){
+        peso = x;
+    }
+    Node<E>* get_From(){
+        return from;
+    }
+    Node<E>* get_To(){
+        return to;
+    }
+    double getWeight(){
+        return peso;
+    }
+    ~Edge(){
+    }
 };
-
-
-
-
-
-#endif //UNTITLED7_EDGE_H
+#endif
